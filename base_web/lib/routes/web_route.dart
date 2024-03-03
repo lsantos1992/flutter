70ae1aabc9016fcbdcbe6807202b968c2466e440
Luiz Santos <luiz.a.dos.santos@accenture.com>
2024-03-03 11:44:11 -0300

    Importando como módulo

--- base_web/lib/routes/web_route.dart
+++ base_web/lib/routes/web_route.dart
@@ -1,16 +1,15 @@
 import 'package:auto_route/auto_route.dart';
 import 'web_route.gr.dart';
-import 'package:micro_app_home/routes/micro_app_home_router.dart';
+import 'package:micro_app_home/routes/micro_app_home_package_router.dart';
 
 @AutoRouterConfig(
-  replaceInRouteName: 'Page,Route',
   deferredLoading: true,
+  modules: [
+    MicroAppHomeModuleRouter,
+  ],
 )
 class BaseAppRouter extends $BaseAppRouter {
   @override
-  List<AutoRoute> get routes => [
-        //AutoRoute(page: HomeRoute.page, path: '/', initial: true),
-        ...MicroAppHomeRouter().routes,
-        // ...MicroAppFormularioRouter().routes,
-      ];
+  // TODO: implement routes
+  List<AutoRoute> get routes => throw UnimplementedError();
 }
